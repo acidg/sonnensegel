b54e00d27a3ecbf2965cd35f8832fc6fd44124f3
Benedikt Schlagberger <benedikt.schlagberger@kiwi-vm.de>
2025-08-03 14:44:48 +0200

    Reduce button debounce for faster button inputs

--- include/constants.h
+++ include/constants.h
@@ -4,7 +4,7 @@
 // Timing Constants
 const unsigned long MOTOR_START_PULSE_MS = 1000;
 const unsigned long MOTOR_STOP_PULSE_MS = 100;
-const unsigned long BUTTON_DEBOUNCE_MS = 50;
+const unsigned long BUTTON_DEBOUNCE_MS = 20;
 const unsigned long BUTTON_LONG_PRESS_MS = 1000;
 const unsigned long WIND_SENSOR_DEBOUNCE_MS = 10;
 const unsigned long POSITION_UPDATE_INTERVAL_MS = 100;
